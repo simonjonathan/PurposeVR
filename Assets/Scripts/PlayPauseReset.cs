448c3bc78f8537b580d7e2c3a8a7639740faf973
simonjonathan <32815627+simonjonathan@users.noreply.github.com>
2018-01-31 16:44:16 +0100

    Scripts
    Scripts of final version. WS 17/18

---
+++ Assets/Scripts/PlayPauseReset.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.Playables;
+
+public class PlayPauseReset : MonoBehaviour {
+
+	private VideoPlayer videoPlayer;
+	public GameObject TV;
+	public PlayableDirector agentTimeline;
+
+	// Use this for initialization
+	void Start () {
+		videoPlayer = GetComponent<VideoPlayer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void Play(){
+		videoPlayer.Play ();
+		agentTimeline.Play ();
+	}
+	public void Pause(){
+		videoPlayer.Pause ();
+		agentTimeline.Pause ();
+	}
+	public void Reset(){
+		videoPlayer.Stop ();
+		agentTimeline.Stop ();
+	}
+	public void OnOffSwitch(){
+		if (!TV.activeSelf)
+			TV.SetActive (true);
+		else
+			TV.SetActive (false);
+		
+	}
+
+}
